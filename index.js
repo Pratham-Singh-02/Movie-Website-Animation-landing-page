d2733fc8a5d923eac02fa7508e1ea387b0224232
Pratham-Singh-02 <iamprathamsingh007@gmail.com>
2024-07-10 23:09:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+
+}
+
+
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const Contents = document.querySelectorAll('.content');
+    banner.style.background = `url("./movies/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    Contents.forEach(content => {
+        content.classList.remove('active');
+        if(content.classList.contains(title)) {
+            content.classList.add('active');
+            
+        }
+    });
+}
